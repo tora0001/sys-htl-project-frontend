bf996de499d90d9fc26dc2ccb4ad5a3678c649fc
Tobias <tobe.2309@hotmail.com>
2024-05-25 17:05:56 +0200

    added overview

--- src/Components/HomepageComponent.tsx
+++ src/Components/HomepageComponent.tsx
@@ -1,25 +1,7 @@
-import { useEffect } from 'react';
-
 const HotelHomePage = () => {
-    useEffect(() => {
-        fetch('http://localhost:8080/room-types')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    }, []);
-
-    return (
-        <h2>Welcome to HTL Hotel Homepage!</h2>
-    );
+        return (
+            <h2>Welcome to HTL Hotel Homepage!</h2>
+        )
 }
 
 export default HotelHomePage;
